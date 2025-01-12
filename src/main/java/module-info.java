c05e8945757c065e642d7ee6f4329d655f66d4c9
Paulem <66211574+Paulem79@users.noreply.github.com>
2025-01-12 15:11:13 +0100

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,13 @@
 module FlowJsonCreator {
     requires atlantafx.base;
-    requires com.google.gson;
     requires java.xml.bind;
     requires javafx.controls;
     requires joptsimple;
     requires methanol;
-    requires org.jetbrains.annotations;
     requires org.slf4j;
-    requires java.desktop;
     requires nativejavafx.taskbar;
+    requires modrinthapi;
+    requires org.jetbrains.annotations;
+    requires com.google.gson;
+    requires java.desktop;
 }
